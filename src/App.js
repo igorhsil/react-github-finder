814f4aeb949f712c0e00626344e1e5291714f74c
igorhsil <igorh@swissinnolab.com>
2022-05-25 08:50:37 +0200

    fix/daisyui cdn

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            hello
+        <div >
+          <h1 className='text-xl'>hello</h1>
+
         </div>
     );
 }
