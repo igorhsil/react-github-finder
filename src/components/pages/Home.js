5543734e083d0f92bf26be61b8a76832ba34988b
igorhsil <igorh@swissinnolab.com>
2022-05-26 13:08:37 +0200

    add/search users component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { Fragment } from "react";
 import UserResults from "../users/UserResults";
+import UserSearch from "../users/UserSearch";
 
 function Home(props) {
   return (
-    <div>
-      <h1 className="text-6xl">
-        {/*  Serach Component  */}
-        <UserResults />
-      </h1>
-    </div>
+    <Fragment>
+      <UserSearch />
+      <UserResults />
+    </Fragment>
   );
 }
 
---
+++ src/components/users/UserSearch.js
@@ -0,0 +1,57 @@
+import React, { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
+
+function UserSearch(props) {
+  const [input, setInput] = useState("");
+
+  const { users } = useContext(GithubContext);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (input === "") {
+      alert("enter something");
+    } else {
+      // @todo -serach user
+
+      setInput("");
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-2 gap-8">
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-control">
+            <div className="relative">
+              <input
+                type="text"
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                placeholder="Search"
+                value={input}
+                onChange={handleInputChange}
+              />
+              <button
+                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
+                type="submit"
+              >
+                Go
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      {users.length > 0 && (
+        <div>
+          <button className="btn-ghost btn-lg">Clear</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default UserSearch;
